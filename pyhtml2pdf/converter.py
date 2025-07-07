9a0dcbd63074728723228db079c7790173b56f60
Danila Valko <danila.valko@proton.me>
2025-07-07 17:16:53 +0200

    Add files via upload

--- pyhtml2pdf/converter.py
+++ pyhtml2pdf/converter.py
@@ -1,7 +1,6 @@
+import sys
 import json
 import base64
-import io
-from typing import Union, TypedDict
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
@@ -12,73 +11,48 @@ from selenium.webdriver.support.expected_conditions import staleness_of
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 
-from .compressor import _compress
-
-
-class PrintOptions(TypedDict):
-    landscape: bool
-    displayHeaderFooter: bool
-    printBackground: bool
-    scale: float
-    paperWidth: float
-    paperHeight: float
-    marginTop: float
-    marginBottom: float
-    marginLeft: float
-    marginRight: float
-    pageRanges: str
-    ignoreInvalidPageRanges: bool
-    preferCSSPageSize: bool
+from .compressor import __compress
 
 
 def convert(
-    source: Union[str, io.BytesIO],
-    target: Union[str, io.BytesIO],
+    source: str,
+    target: str,
     timeout: int = 2,
     compress: bool = False,
     power: int = 0,
     install_driver: bool = True,
-    print_options: PrintOptions = {},
-    ghostscript_command: str = None
+    print_options: dict = {},
 ):
     """
     Convert a given html file or website into PDF
 
-    :param str source: source html file or website link or html content or a BytesIO object
-    :param str | BytesIO target: target location to save the PDF, can be a path or a BytesIO object
+    :param str source: source html file or website link
+    :param str target: target location to save the PDF
     :param int timeout: timeout in seconds. Default value is set to 2 seconds
     :param bool compress: whether PDF is compressed or not. Default value is False
     :param int power: power of the compression. Default value is 0. This can be 0: default, 1: prepress, 2: printer, 3: ebook, 4: screen
     :param bool install_driver: whether or not to install using ChromeDriverManager. Default value is True
-    :param PrintOptions print_options: A dictionary containing options for the printing of the PDF, conforming to the types specified in the PrintOptions TypedDict.
-    :param ghostscript_command: The name of the ghostscript executable. If set to the default value None, is attempted
-                            to be inferred from the OS.
-                            If the OS is not Windows, "gs" is used as executable name.
-                            If the OS is Windows, and it is a 64-bit version, "gswin64c" is used. If it is a 32-bit
-                            version, "gswin32c" is used.
+    :param dict print_options: options for the printing of the PDF. This can be any of the params in here:https://vanilla.aslushnikov.com/?Page.printToPDF
     """
-    if print_options is None:
-        print_options = {}
 
     result = __get_pdf_from_html(
         source, timeout, install_driver, print_options)
 
     if compress:
-        _compress(result, target, power, ghostscript_command)
+        __compress(result, target, power)
     else:
-        if type(target) == io.BytesIO:
-            return target.write(result)
         with open(target, "wb") as file:
             file.write(result)
 
 
-def __send_devtools(driver, cmd, params=None):
-    if params is None:
-        params = {}
+def __send_devtools(driver, cmd, params={}):
     resource = "/session/%s/chromium/send_command_and_get_result" % driver.session_id
-    url = driver.command_executor._url + resource
+    #url = driver.command_executor._url + resource
+   
+    url = f"{driver.command_executor._client_config.remote_server_addr}{resource}"
+
     body = json.dumps({"cmd": cmd, "params": params})
-    response = driver.command_executor._request("POST", url, body)
+    response = driver.command_executor._request("POST", url , body)
 
     if not response:
         raise Exception(response.get("value"))
@@ -87,11 +61,11 @@ def __send_devtools(driver, cmd, params=None):
 
 
 def __get_pdf_from_html(
-    source: Union[str, io.BytesIO], timeout: int, install_driver: bool, print_options: dict
+    path: str, timeout: int, install_driver: bool, print_options: dict
 ):
-) -> bytes:
     webdriver_options = Options()
     webdriver_prefs = {}
+    driver = None
 
     webdriver_options.add_argument("--headless")
     webdriver_options.add_argument("--disable-gpu")
@@ -106,17 +80,6 @@ def __get_pdf_from_html(
         driver = webdriver.Chrome(service=service, options=webdriver_options)
     else:
         driver = webdriver.Chrome(options=webdriver_options)
-
-    
-    # Detect the type of source and create data url if needed
-    if type(source) == io.BytesIO:
-        encoded_content = base64.b64encode(source.getvalue()).decode('utf-8')
-        path = f'data:text/html;base64,{encoded_content}'
-    if not source.startswith('http') and not source.startswith('file'):
-        encoded_content = base64.b64encode(source.encode('utf-8')).decode('utf-8')
-        path = f'data:text/html;base64,{encoded_content}'
-    else:
-        path = source
 
     driver.get(path)
 
@@ -134,6 +97,5 @@ def __get_pdf_from_html(
         calculated_print_options.update(print_options)
         result = __send_devtools(
             driver, "Page.printToPDF", calculated_print_options)
-        return base64.b64decode(result["data"])
-    finally:
         driver.quit()
+        return base64.b64decode(result["data"])
